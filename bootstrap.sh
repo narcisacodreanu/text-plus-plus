2df18c891635bd98c8cc5d9290e8f6f5f3537a93
aizawak <ka2532@columbia.edu>
2015-03-07 19:19:10 -0500

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install -y python
+sudo apt-get install -y python-pip
+sudo apt-get install -y vim
+sudo pip install flask
+sudo pip install requests
+
+
+echo hello
