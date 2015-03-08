45fefcaed45bf4c2e1322c1e06bab3ca38453d3f
aizawak <ka2532@columbia.edu>
2015-03-07 22:43:55 -0500

    scrapes rhymezone

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
-__author__ = 'Ken'
-# -*- coding: utf-8 -*-
+__author__ = 'Ken and Narcisa'
 from flask import Flask, render_template, request
-import requests
 from bs4 import BeautifulSoup
-
+from urllib2 import urlopen
+import re
+import random
 
 app = Flask(__name__)
-app.config["DEBUG"] = True  # Only include this while you are testing your app
+app.config["DEBUG"] = False  # Only include this while you are testing your app
 
 sampleText = "Hey what are you doing tonight?"
 
+def splitText(text):
+	return text.split()
 
+def scrapeRhymeZone():
+	words = splitText(sampleText.replace("?",""))
+	baseURL = "http://www.rhymezone.com/r/rhyme.cgi?Word="
+	endURL = "&org1=syl&org2=l&org3=y&typeofrhyme=perfect"
+	finalString = ""
+	for i in words:
+		soup = BeautifulSoup(urlopen(baseURL + i + endURL))
+		soup.select(".d~ a")
+		soup.select(".d")
+		rhymingWords = soup.select("font b a")
+		finalRhymingWords = []
+		for i in rhymingWords:
+			result = re.sub(str("<.*?>"), str(""), str(i))
+			result = result.replace("\xc2\xa0", " ")
+			if not " " in result:
+				finalRhymingWords.append(result)	
+		finalString += finalRhymingWords[random.randint(0,len(finalRhymingWords)-1)] + " "
+	print(finalString)
 
+scrapeRhymeZone()
+
+#.d~ a , .d , font b a
 
 @app.route("/")
 def home():
