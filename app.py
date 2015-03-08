9f83b34d3505e96d2872ad6c8a13b3cfd61e4312
aizawak <ka2532@columbia.edu>
2015-03-07 20:58:27 -0500

    updated bootstrap

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ __author__ = 'Ken'
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request
 import requests
+from bs4 import BeautifulSoup
+
 
 app = Flask(__name__)
 app.config["DEBUG"] = True  # Only include this while you are testing your app
 
-def fun():
-	as;ldjf
+sampleText = "Hey what are you doing tonight?"
+
+
+
 
 @app.route("/")
 def home():
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y python-pip
 sudo apt-get install -y vim
 sudo pip install flask
 sudo pip install requests
-
+sudo pip install beautifulsoup4
 
 echo hello
